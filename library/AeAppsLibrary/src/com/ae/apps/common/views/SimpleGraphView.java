68a1d3c1dcd8834eadc4fb420d2b3b17b1d2bcf0
midhun harikumar <midhunhk@gmail.com>
2013-08-03 22:48:57 +0530

    Optimizations for Chart Display

--- library/AeAppsLibrary/src/com/ae/apps/common/views/SimpleGraphView.java
+++ library/AeAppsLibrary/src/com/ae/apps/common/views/SimpleGraphView.java
@@ -1,6 +1,7 @@
 package com.ae.apps.common.views;
 
 import android.content.Context;
+import android.content.res.Configuration;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
@@ -21,18 +22,20 @@ public class SimpleGraphView extends View {
 	/**
 	 * If a theme is not provided, this will be used
 	 */
-	private static int[]	DEFAULT_THEME	= { Color.LTGRAY, Color.YELLOW, Color.GREEN, Color.GRAY, Color.BLACK,
+	private static int[]		DEFAULT_THEME	= { Color.LTGRAY, Color.YELLOW, Color.GREEN, Color.GRAY, Color.BLACK,
 			Color.RED, Color.DKGRAY, Color.CYAN };
-
-	private float[]			value_degree	= null;
-	private String[]		labels			= null;
-	private int[]			colors			= null;
-	private float			temp			= 0;
-	private Paint			paint			= new Paint(Paint.ANTI_ALIAS_FLAG);
-	private RectF			rectf			= new RectF(10, 10, 250, 250);
+	private static final int	PADDING			= 10;
+	private float[]				value_degree	= null;
+	private String[]			labels			= null;
+	private int[]				colors			= null;
+	private float				temp			= 0;
+	private Paint				mChartPaint		= new Paint(Paint.ANTI_ALIAS_FLAG);
+	private Paint				mTextPaint		= new Paint(Paint.ANTI_ALIAS_FLAG);
+	private RectF				mBoundingRect	= new RectF(PADDING, PADDING, 250, 250);
 
 	public SimpleGraphView(Context context, float[] values, int[] themeColors) {
 		super(context);
+
 		// Check if a valid themeColors array is passed to us
 		if (themeColors != null && themeColors.length > 0) {
 			colors = themeColors;
@@ -43,6 +46,8 @@ public class SimpleGraphView extends View {
 		for (int i = 0; i < values.length; i++) {
 			value_degree[i] = values[i];
 		}
+		// Set FontSize based on scale
+		mTextPaint.setTextSize(12 * getResources().getDisplayMetrics().density);
 	}
 
 	/**
@@ -95,24 +100,42 @@ public class SimpleGraphView extends View {
 
 		for (int i = 0; i < value_degree.length; i++) {
 			if (i == 0) {
-				paint.setColor(colors[i % colors.length]);
-				canvas.drawArc(rectf, 0, value_degree[i], true, paint);
+				mChartPaint.setColor(colors[i % colors.length]);
+				canvas.drawArc(mBoundingRect, 0, value_degree[i], true, mChartPaint);
 			} else {
 				temp += value_degree[i - 1];
-				paint.setColor(colors[i % colors.length]);
-				canvas.drawArc(rectf, temp, value_degree[i], true, paint);
+				mChartPaint.setColor(colors[i % colors.length]);
+				canvas.drawArc(mBoundingRect, temp, value_degree[i], true, mChartPaint);
 			}
 		}
 
 		if (labels != null && labels.length > 0) {
 			// Draw the labels
-			int xCoordinate = 15;
-			int yCoordinate = 260;
+			int xCoordinate = (int) (PADDING * 1.5);
+			int yCoordinate = (int) mBoundingRect.right + PADDING;
 			for (int i = 0; i < labels.length; i++) {
-				paint.setColor(colors[i % colors.length]);
-				canvas.drawText(labels[i], yCoordinate, xCoordinate, paint);
-				xCoordinate += 25;
+				mTextPaint.setColor(colors[i % colors.length]);
+				canvas.drawText(labels[i], yCoordinate, xCoordinate, mTextPaint);
+				xCoordinate += 30;
 			}
 		}
 	}
+
+	@Override
+	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+		int specWidth = MeasureSpec.getSize(widthMeasureSpec);
+		int specHeight = MeasureSpec.getSize(heightMeasureSpec);
+		int rectSize = 0;
+		if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
+			// We are going to divide the screen width into two for chart and the text block
+			rectSize = Math.min(specHeight, specWidth / 2);
+		} else {
+			// For landscape, use 65% height instead of 50%
+			rectSize = (int) (specHeight * 0.65);
+		}
+		// This rectangle turns into a square and the chart is drawn on it
+		mBoundingRect.set(PADDING, PADDING, rectSize, rectSize);
+		setMeasuredDimension(specWidth, rectSize + PADDING);
+	}
+
 }
