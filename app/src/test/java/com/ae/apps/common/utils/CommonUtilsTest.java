4a219eb6523aed1c219c72109aecda1b4a659e57
midhunhk <midhunhk@gmail.com>
2017-06-24 02:23:57 +0530

    add CommonUtilsTest

---
+++ app/src/test/java/com/ae/apps/common/utils/CommonUtilsTest.java
@@ -0,0 +1,23 @@
+package com.ae.apps.common.utils;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+/**
+ * Created by user on 6/24/2017.
+ */
+
+public class CommonUtilsTest {
+
+    @Test
+    public void testTruncateString() {
+        assertEquals("", CommonUtils.truncateString(null));
+        assertNotNull(CommonUtils.truncateString(null));
+
+        assertEquals("Hello", CommonUtils.truncateString("Hello"));
+        assertEquals("abcdefghijklmnopqrstuvwxyz...",
+                CommonUtils.truncateString("abcdefghijklmnopqrstuvwxyz1234"));
+    }
+}
