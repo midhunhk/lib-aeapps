6b20ae50db3d295acd1bc7939529641c1eec9938
midhunhk <midhunhk@gmail.com>
2018-09-22 19:36:38 -0400

    add new api for sms

---
+++ modules/core/src/main/java/com/ae/apps/lib/api/sms/SmsApiCostants.java
@@ -0,0 +1,4 @@
+package com.ae.apps.lib.api.sms;
+
+public interface SmsApiContacts {
+}
---
+++ modules/core/src/main/java/com/ae/apps/lib/api/sms/SmsApiGatewayImpl.java
@@ -0,0 +1,6 @@
+package com.ae.apps.lib.api.sms;
+
+import SmsApiGateway;
+
+public class SmsApiGatewayImpl implements SmsApiGateway {
+}
