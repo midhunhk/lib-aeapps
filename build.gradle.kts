0895aed500dd3fba8a1711ab6cd72878fa894592
midhunhk <2026817+midhunhk@users.noreply.github.com>
2024-09-03 15:59:55 -0400

    gcp update

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,8 +6,8 @@ plugins {
      * see Applying external plugins with same version to subprojects.
      */
 
-    id("com.android.application") version "8.5.2" apply false
-    id("com.android.library") version "8.5.2" apply false
+    id("com.android.application") version "8.6.0" apply false
+    id("com.android.library") version "8.6.0" apply false
     id("org.jetbrains.kotlin.android") version "2.0.0" apply false
     id("maven-publish")
 }
