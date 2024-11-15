a3c829ae17d906e76d2abf1e064f450a800f96b6
midhunhk <2026817+midhunhk@users.noreply.github.com>
2024-11-15 14:03:48 -0500

    fix build version

--- buildSrc/src/main/java/com/ae/apps/lib/Publish.kt
+++ buildSrc/src/main/java/com/ae/apps/lib/Publish.kt
@@ -2,13 +2,13 @@ package com.ae.apps.lib
 
 object Publish {
     const val GROUP_ID = "com.github.midhunhk.lib-aeapps"
-    const val VERSION_STRING = "5.0.0-beta.01"
+    const val VERSION_STRING = "5.0.0-beta.02"
 }
 
 object ArtifactId {
     const val CORE = "core"
     const val CONTACTS_API = "contacts-api"
-    const val SMS_API = "sms_api"
+    const val SMS_API = "sms-api"
     const val RUNTIME_PERMISSIONS = "runtime-permissions"
     const val MULTI_CONTACT = "multi-contact"
     const val DATABASE_HELPERS = "database-helpers"
